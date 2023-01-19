24cc59a72ebf671f943722e5a23ad828f30bde95
Veny Cordova <venycordova@gmail.com>
2023-01-18 20:02:32 -0500

    adding a dark a light mode to the webpage by using redux useSelector

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import './App.css';
 import Nav from './components/Nav'
 import ContentWrapper from './components/ContentWrapper'
 import Footer from './components/Footer'
+import { darkMode, lightMode } from './features/modeSlice';
+//import useselector abd usedispatch, as well as dark and light mode
+import { useSelector, useDispatch } from 'react-redux';
 
 function App() {
+  //define our dipatcha and selector
+  const dispatch = useDispatch()
+  const mode = useSelector((state) => state.mode)
   
+  //create a function to control the toggle, dark or light more
+  const toggleMode = () => {
+    mode.darkMode ? dispatch(lightMode()) : dispatch(darkMode())
+  }
+  //create a button to controle the toggle 
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
+    <div style={{ backgroundColor: mode.color1, color: 'white' }} className="App">
       <Nav />
+      <button onClick={toggleMode}>{ mode.darkMode ? 'Light Mode' : 'Dark Mode' }</button>
       <ContentWrapper />
       <Footer />
     </div>
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,8 +1,10 @@
 import React from 'react'
-function Photo() {
+import { useSelector } from 'react-redux'
 
+function Photo() {
+    const mode = useSelector((state)=> state.mode)
     return (
-        <div className="Photo">
+        <div className="Photo" style={{ 'backgroundColor': mode.color3 }}>
             <img src='https://placekitten.com/200/300' alt="author" />
         </div>
     )
